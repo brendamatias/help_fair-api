1c49f8221364123983d0d3f87ef3ea4ecf3399b2
Brenda Matias <brendamatias.sobral@gmail.com>
2023-07-30 19:07:46 -0300

    feat: order by bought

--- src/mediators/FairProducts/All.ts
+++ src/mediators/FairProducts/All.ts
@@ -3,7 +3,9 @@ import responses from '@/config/response';
 import FairProduct from '@/models/FairProduct';
 
 export default async (fair: string) => {
-  const products = await FairProduct.find({ fair: new ObjectId(fair) }).exec();
+  const products = await FairProduct.find({ fair: new ObjectId(fair) })
+    .sort({ bought: 1 })
+    .exec();
 
   return responses.success(products);
 };
