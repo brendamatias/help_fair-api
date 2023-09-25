ad09bde2fc2380979e7ca31bff7f1d5718d4ff5a
Brenda Matias <brendamatias.sobral@gmail.com>
2023-09-25 01:25:08 -0300

    feat: add total value

--- src/mediators/Fairs/All.ts
+++ src/mediators/Fairs/All.ts
@@ -1,8 +1,39 @@
+import mongoose from 'mongoose';
 import responses from '../../config/response';
 import Fair from '../../models/Fair';
 
 export default async (userId: string) => {
-  const fairs = await Fair.find({ userId, active: true }).exec();
+  const fairs = await Fair.aggregate([
+    { $match: { active: true, userId: new mongoose.Types.ObjectId(userId) } },
+    {
+      $lookup: {
+        from: 'fairproducts',
+        localField: '_id',
+        foreignField: 'fair',
+        pipeline: [
+          {
+            $project: {
+              total: {
+                $multiply: ['$price', '$qty'],
+              },
+            },
+          },
+        ],
+        as: 'result',
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        status: 1,
+        active: 1,
+        createdAt: 1,
+        total: {
+          $sum: '$result.total',
+        },
+      },
+    },
+  ]).exec();
 
   return responses.success(fairs);
 };
